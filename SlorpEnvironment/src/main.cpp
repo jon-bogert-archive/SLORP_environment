a11b6b6b7c7c0564e3a79b6e9b8c462320455c74
jon-bogert <jonbogert@gmail.com>
2021-12-17 21:05:36 -0800

    Init Commit
    Basic project setup

---
+++ SlorpEnvironment/src/main.cpp
@@ -0,0 +1,17 @@
+//system headers for Debug
+#include <iostream>
+
+//Project Headers
+#include "raylib.h"
+
+
+//system namespace for Debug
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+	system("pause>0");
+
+	return 0;
+}
